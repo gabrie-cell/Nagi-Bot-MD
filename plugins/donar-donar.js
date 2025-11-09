d0e4e5854fe5b1e4debd0794987cc38e308ba295
Gabrie-ux <gabrielrimachi214@gmail.com>
2025-11-09 13:49:56 -0500

    Actualizar donar-donar.js

--- plugins/donar-donar.js
+++ plugins/donar-donar.js
@@ -3,7 +3,7 @@ var handler = async (m, { conn, command }) => {
 
   let str = `*Hola ${user} (⁠◜⁠‿⁠◝⁠)⁠♡*
 
-Si estás disfrutando de nuestro Botsito y deseas apoyarnos, cualquier aporte pequeño que sea es muy bienvenido 💖
+Si estás disfrutando de nuestra Botsita y deseas apoyarnos, cualquier aporte pequeño que sea es muy bienvenido 💖
 
 ❤️‍🔥 *Puedes donar a través de PayPal:*  
 copiakevin22@gmail.com
@@ -11,10 +11,10 @@ copiakevin22@gmail.com
 🫂 Gracias por tu apoyo, Eres increíble (⁠◕‿◕)⁠ノ
 
 🔗 *Repositorio del bot:*  
-github.com/Eliasivan/Erza2-bot-MD
+github.com/gabrie-ux/Erza2-bot-MD
 
 🧑🏻‍💻 *Creador:*  
-https://wa.me/595972157130`
+https://wa.me/51941247696`
 
   conn.reply(m.chat, str, m, { mentions: [m.sender] })
 }
